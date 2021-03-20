0f16ba0511c37f759ba76c84bef9004e18b1e48b
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-03-20 20:54:45 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type = string
+  default = ""
+}
+
+variable "parent_authority_path" {
+  type = string
+  default = ""
+}
+
+variable "urls_prefix" {
+  type = set(string)
+  default = []
+}
